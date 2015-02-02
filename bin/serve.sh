4f4e92ae14fc741e3367332648b0f971503d672c
Christoph Wagner <chwagner@gmail.com>
2015-02-02 17:08:51 +1000

    Initial checkin

---
+++ bin/serve.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Starts a basic web server on the port specified.
+# 
+# ./serve.sh 3000 -> http://localhost:3000
+#
+# Copyright 2012 Eric Bidelman <ebidel@gmail.com>
+# Copyright 2014 Christoph Wagner <christoph@pandastrike.com>
+
+port=$1
+if [ $#  -ne  1 ]
+then
+  port=1337
+fi
+
+if [ $(uname -s) == "Darwin" ]
+then
+  open=open
+else
+  open=xdg-open
+fi
+
+(sleep 2; $open http://localhost:$port/slides.html) &
+./node_modules/.bin/harp server -p $port
